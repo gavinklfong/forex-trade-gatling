fef4bc562b7aa62cfad0ff42761e7a463f09ae15
Gavin Fong <gavinklfong@gmail.com>
2021-07-11 17:20:51 +0100

    updated volume profile

--- src/test/scala/forex/ForexSimulation.scala
+++ src/test/scala/forex/ForexSimulation.scala
@@ -8,7 +8,8 @@ import io.gatling.http.Predef._
 
 class ForexSimulation extends Simulation {
 
-  val API_HOST = "localhost"
+//  val API_HOST = "localhost"
+  val API_HOST = "192.168.30.111"
   val API_PORT = "8080"
 
   val httpProtocol = http
@@ -58,17 +59,22 @@ class ForexSimulation extends Simulation {
 
   val scn = scenario("ForexSimulation")
     .randomSwitch (
-      60d -> exec(UserJourneys.browseForexRates),
+      30d -> exec(UserJourneys.browseForexRates),
       30d -> exec(UserJourneys.browseForexRatesAndBookRate),
-      10d -> exec(UserJourneys.browseForexRatesAndSubmitForexTradeDeal)
+      40d -> exec(UserJourneys.browseForexRatesAndSubmitForexTradeDeal)
     )
 
 
   setUp(
     scn.inject(
-      constantUsersPerSec(5) during (1.minute)
+      constantUsersPerSec(10).during(15.seconds),
+      rampUsersPerSec(10).to(40).during(15.seconds),
+      constantUsersPerSec(40).during(30.seconds),
+//      heavisideUsers(100).during(30.seconds)
+
 //        constantConcurrentUsers(10).during(10.seconds),
-//        rampConcurrentUsers(10).to(20).during(10.seconds)
+//        rampConcurrentUsers(10).to(50).during(20.seconds),
+//        constantConcurrentUsers(50).during(30.seconds),
 
 //      incrementConcurrentUsers(5) // Int
 //        .times(5)
